e6b0b7b065c39dac2dbd457a3f713ab956462e7b
William Mosquera <wjmmk3@gmail.com>
2025-02-22 16:53:47 -0500

    feat: initial start-app done

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { envs } from './config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const logger = new Logger('Main');
+  const reset = '\x1b[33m';
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: { port: envs.port },
+    },
+  );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true
+    })
+  );
+
+  
+  await app.listen();
+  logger.log(`Orders MicroService running on port: ${reset}${envs.port}`)
 }
 bootstrap();
